3ad3fb6cc509f7abb1e2861aea221c4c1e278c6f
Shubham Patel <Shubham.Patel@einfochips.com>
2023-02-22 12:23:39 +0530

    for loop

---
+++ demo8_loops/demo2_for_each_loop.py
@@ -0,0 +1,9 @@
+numbers=[45,98,75,65,24,88,74,56,75]
+
+
+for i in numbers:
+    print(i)
+
+colors = ["red", "green", "yellow", "blue", "green", "black", "grey"]
+for col in colors:
+    print(col)
