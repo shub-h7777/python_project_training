e6b48e212175507260f0d79a11f2d1d24c28b8fb
Shubham Patel <Shubham.Patel@einfochips.com>
2023-02-22 11:50:47 +0530

    Added for loop demo-8 package

---
+++ demo8_loops/demo1.py
@@ -0,0 +1,13 @@
+colors = ["red", "green", "yellow", "blue", "green"]
+for i in range(0,5):
+    print(colors[i])
+
+# for i in range(0,len(colors)):
+#     print(colors[i],end="")
+
+numbers=[45,98,75,65,24,88,74,56,75]
+for i in range(0,len(numbers)):
+    if numbers[i]<=50:
+        print(numbers[i])
+
+
